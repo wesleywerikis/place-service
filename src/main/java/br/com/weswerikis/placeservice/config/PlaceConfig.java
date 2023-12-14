7bad2281508944c873df1024734988911f067a28
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-14 11:20:43 -0300

    Added @EnableR2dbcAuditing annotation to enable R2DBC auditing in PlaceConfig class

--- src/main/java/br/com/weswerikis/placeservice/config/PlaceConfig.java
+++ src/main/java/br/com/weswerikis/placeservice/config/PlaceConfig.java
@@ -2,11 +2,13 @@ package br.com.weswerikis.placeservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
 import br.com.weswerikis.placeservice.domain.PlaceRepository;
 import br.com.weswerikis.placeservice.domain.PlaceService;
 
 @Configuration
+@EnableR2dbcAuditing
 public class PlaceConfig {
     
     @Bean

aaec081f87c0bef5a2546b899493adbf41279cf8
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-14 14:17:17 -0300

    Initialized Slugify in PlaceService constructor, and modified the creation of the Place entity to include the slug generated using Slugify for the name field

--- src/main/java/br/com/weswerikis/placeservice/domain/PlaceService.java
+++ src/main/java/br/com/weswerikis/placeservice/domain/PlaceService.java
@@ -1,18 +1,22 @@
 package br.com.weswerikis.placeservice.domain;
 
+import com.github.slugify.Slugify;
+
 import br.com.weswerikis.placeservice.api.PlaceRequest;
 import reactor.core.publisher.Mono;
 
 public class PlaceService {
     private PlaceRepository placeRepository;
+    private Slugify slg;
 
     public PlaceService(PlaceRepository placeRepository) {
         this.placeRepository = placeRepository;
+        this.slg = Slugify.builder().build();
     }
 
     public Mono<Place> create(PlaceRequest placeRequest){
-        var place = new Place(null, placeRequest.name(), placeRequest.slug(),
-         placeRequest.state(), placeRequest.createdAt(), placeRequest.updatedAt());
+        var place = new Place(null, placeRequest.name(), slg.slugify(placeRequest.name()),
+         placeRequest.state(), null,null);
         return placeRepository.save(place);
     }
 }

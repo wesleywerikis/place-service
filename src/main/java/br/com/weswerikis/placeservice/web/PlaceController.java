d775c94fca624d8e2a4af609b0a0c34918902fb2
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-14 10:17:21 -0300

    Added PlaceController with create endpoint for handling Place creation requests

---
+++ src/main/java/br/com/weswerikis/placeservice/web/PlaceController.java
@@ -0,0 +1,28 @@
+package br.com.weswerikis.placeservice.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.weswerikis.placeservice.domain.Place;
+import br.com.weswerikis.placeservice.domain.PlaceService;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/places")
+public class PlaceController {
+    private PlaceService placeService;
+
+    public PlaceController(PlaceService placeService) {
+        this.placeService = placeService;
+    }
+
+    @PostMapping
+    public ResponseEntity <Mono<Place>> create(Place place){
+        var createdPlace = placeService.create(place);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdPlace);
+    }
+
+}

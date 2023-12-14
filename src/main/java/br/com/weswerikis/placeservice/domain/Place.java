14756a362434845cb3340d2f9d51e08aa0d19c26
Wesley Werikis <wesleywerikis@outlook.com>
2023-12-14 09:43:40 -0300

    Added Place record class with id, name, slug, state, createdAt, and updatedAt fields

---
+++ src/main/java/br/com/weswerikis/placeservice/domain/Place.java
@@ -0,0 +1,9 @@
+package br.com.weswerikis.placeservice.domain;
+
+import java.time.LocalDateTime;
+
+public record Place(
+
+    Long id, String name, String slug, String state, LocalDateTime createdAt, LocalDateTime updatedAt) {
+    
+}
